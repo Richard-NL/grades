d300326710b25ee8f2d8c6a3c29136c027c705e3
Richard Hoogstad <rhoogstad@saa.nl>
2020-11-01 12:58:01 +0100

    Use formula for grade calculation

--- src/Grades/GradeCalculator.php
+++ src/Grades/GradeCalculator.php
@@ -4,6 +4,9 @@ namespace App\Grades;
 
 class GradeCalculator
 {
+    private const GRADE_INCREMENT_1 = 0.09;
+    private const GRADE_INCREMENT_2 = 0.15
+    ;
     public function calculatePercentage(int $maxScore, int $score): float
     {
         return (float) $score / ($maxScore / 100);
@@ -11,13 +14,14 @@ class GradeCalculator
 
     public function calculateGrade(float $percentageCorrect): float
     {
-        if ($percentageCorrect <= 20) {
+        if ($percentageCorrect < 20) {
             return 1.0;
         }
 
-        if ($percentageCorrect <= 70) {
-            return 5.5;
+        if ($percentageCorrect >= 20 && $percentageCorrect < 70) {
+            return 1.0 + ($percentageCorrect - 20) * self::GRADE_INCREMENT_1;
         }
-        return 10;
+
+        return 5.5 + ($percentageCorrect - 70) * self::GRADE_INCREMENT_2;
     }
 }
