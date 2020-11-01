2733d52ed749684591e383f3a4858c001ea51993
Richard Hoogstad <rhoogstad@saa.nl>
2020-11-01 18:49:09 +0100

    Added calculated values for statistics

--- src/Question/QuestionStatisticProcessor.php
+++ src/Question/QuestionStatisticProcessor.php
@@ -2,15 +2,57 @@
 
 namespace App\Question;
 
+use App\Grade\GradeResultProcessor;
+use App\Spreadsheet\ExcelReader;
+use App\Spreadsheet\SpreadsheetDataFormatter;
+
 class QuestionStatisticProcessor
 {
+
+    private ExcelReader $excelReader;
+
+    private QuestionCalculator $questionCalculator;
+
+    private SpreadsheetDataFormatter $spreadsheetDataFormatter;
+
+    private GradeResultProcessor $gradeResultProcessor;
+
+    public function __construct(
+        ExcelReader $excelReader,
+        QuestionCalculator $questionCalculator,
+        SpreadsheetDataFormatter $spreadsheetDataFormatter,
+        GradeResultProcessor $gradeResultProcessor
+    ) {
+        $this->excelReader = $excelReader;
+        $this->questionCalculator = $questionCalculator;
+        $this->spreadsheetDataFormatter = $spreadsheetDataFormatter;
+        $this->gradeResultProcessor = $gradeResultProcessor;
+    }
+
     public function questionStatistics(): array
     {
-        return [
-            'Question 1' => ['p' => 1, 'r' => 1],
-            'Question 2' => ['p' => 0, 'r' => 0],
-            'Question 3' => ['p' => 0.1,'r' => -1],
-            'Question 4' => ['p' => 0.5,'r' => 0.55],
-        ];
+        $spreadSheetData = $this->excelReader->getSpreadSheetData();
+        $studentScores = $spreadSheetData->studentScores();
+        $questionScoresPerStudent = $this->spreadsheetDataFormatter->questionScoresPerStudent($studentScores);
+        $studentGrades = $this->gradeResultProcessor->studentGrades();
+
+        $questionStatistics = [];
+        foreach ($spreadSheetData->questionMaxScores() as $questionName => $questionMaxScore) {
+
+            $pValue = $this->questionCalculator->pValue(
+                $questionMaxScore,
+                ...array_values($questionScoresPerStudent[$questionName])
+            );
+
+            $rValue = $this->questionCalculator->rValue(
+                array_values($questionScoresPerStudent[$questionName]),
+                array_values($studentGrades)
+            );
+
+            $questionStatistics[$questionName] = ['p' => $pValue, 'r' => $rValue];
+        }
+
+        return $questionStatistics;
+
     }
 }
