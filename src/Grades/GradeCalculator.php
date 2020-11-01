b9168df3696a59c3e3974df741215eb673a3b9fb
Richard Hoogstad <rhoogstad@saa.nl>
2020-11-01 12:29:41 +0100

    Start of grade calculation added

--- src/Grades/GradeCalculator.php
+++ src/Grades/GradeCalculator.php
@@ -8,4 +8,16 @@ class GradeCalculator
     {
         return (float) $score / ($maxScore / 100);
     }
+
+    public function calculateGrade(float $percentageCorrect): float
+    {
+        if ($percentageCorrect <= 20) {
+            return 1.0;
+        }
+
+        if ($percentageCorrect <= 70) {
+            return 5.5;
+        }
+        return 10;
+    }
 }
--- tests/Grades/GradeCalculatorTest.php
+++ tests/Grades/GradeCalculatorTest.php
@@ -16,6 +16,15 @@ class GradeCalculatorTest extends TestCase
         $this->assertSame($expected, round($gradeCalculator->calculatePercentage($maxScore, $score), 1));
     }
 
+    /**
+     * @dataProvider calculateGradeProvider
+     */
+    public function testCalculateGrade(float $expected, float $percentageCorrect): void
+    {
+        $gradeCalculator = new GradeCalculator();
+        $this->assertSame($expected, $gradeCalculator->calculateGrade($percentageCorrect), 1);
+    }
+
     public function calculatePercentageProvider(): array
     {
         return [
@@ -24,6 +33,15 @@ class GradeCalculatorTest extends TestCase
             [25, 200, 50],
             [1, 400, 4],
             [33.3, 90, 30],
+        ];
+    }
+
+    public function calculateGradeProvider(): array
+    {
+        return [
+            [1.0, 20],
+            [5.5, 70],
+            [10.0, 100],
         ];
     }
 }
