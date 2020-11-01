8cbe0aff8cb81988c12bcc8e6c0c0410ebaecf73
Richard Hoogstad <rhoogstad@saa.nl>
2020-11-01 18:37:50 +0100

    Changed dummy data to better represent actual result

--- src/Spreadsheet/SpreadSheetData.php
+++ src/Spreadsheet/SpreadSheetData.php
@@ -9,14 +9,55 @@ class SpreadSheetData
         return 100;
     }
 
+    public function questionMaxScores(): array
+    {
+        return [
+            'Question 1' => 100,
+            'Question 2' => 100,
+            'Question 3' => 100,
+            'Question 4' => 100,
+            'Question 5' => 100,
+        ];
+    }
+
     public function studentScores(): array
     {
         return [
-            'Student 1' => [0, 0, 0, 0, 0],
-            'Student 2' => [20, 0, 0, 0, 0],
-            'Student 3' => [69, 0, 0, 0, 0],
-            'Student 4' => [20, 20, 20, 10, 0],
-            'Student 5' => [20, 20, 20, 20, 20],
+            'Student 1' => [
+                'Question 1' => 0,
+                'Question 2' => 0,
+                'Question 3' => 0,
+                'Question 4' => 0,
+                'Question 5' => 0,
+            ],
+            'Student 2' => [
+                'Question 1' => 20,
+                'Question 2' => 0,
+                'Question 3' => 0,
+                'Question 4' => 0,
+                'Question 5' => 0,
+            ],
+            'Student 3' => [
+                'Question 1' => 69,
+                'Question 2' => 0,
+                'Question 3' => 0,
+                'Question 4' => 0,
+                'Question 5' => 0,
+            ],
+            'Student 4' => [
+                'Question 1' => 20,
+                'Question 2' => 20,
+                'Question 3' => 20,
+                'Question 4' => 10,
+                'Question 5' => 0,
+            ],
+            'Student 5' => [
+                'Question 1' => 20,
+                'Question 2' => 20,
+                'Question 3' => 20,
+                'Question 4' => 20,
+                'Question 5' => 20,
+            ],
         ];
     }
 }
