479f0123a0161e30bac632f668537846e89a624a
Richard Hoogstad <rhoogstad@saa.nl>
2020-11-01 16:42:24 +0100

    Added Library for R calculation

--- src/Question/QuestionCalculator.php
+++ src/Question/QuestionCalculator.php
@@ -2,11 +2,25 @@
 
 namespace App\Question;
 
+use MathPHP\Statistics\Correlation;
+
 class QuestionCalculator
 {
+    private Correlation $correlation;
+
+    public function __construct(Correlation $correlation)
+    {
+        $this->correlation = $correlation;
+    }
+    
     public function pValue(int $maxScore, float ...$studentGrades): float
     {
         $average = array_sum(array_values($studentGrades)) / count($studentGrades);
         return round($average /$maxScore, 2);
+    }
+
+    public function rValue(array $questionScorePerStudent, array $studentGrades): float
+    {
+        return $this->correlation->r($questionScorePerStudent, $studentGrades);
     }
 }
